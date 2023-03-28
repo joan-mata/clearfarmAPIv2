b63374752899d8a7fb1e79575f19e9ac75e35f0f
joan-mata <joan.mata.jmp@gmail.com>
2023-03-28 16:02:54 +0200

    GET /search

--- app/search/resources/search.py
+++ app/search/resources/search.py
@@ -3,7 +3,7 @@ from flask import Flask
 from .. import search_bp
 
 
-@search_bp.route('/search')
+@search_bp.route('/search', methods=['GET'])
 def search():
     '''
     Presentation to REST-API.
