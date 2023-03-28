ad771ffd5b95d382d59890baac6ea64d1cf9fd17
joan-mata <joan.mata.jmp@gmail.com>
2023-03-28 16:55:01 +0200

    update comments /search

--- app/search/resources/search.py
+++ app/search/resources/search.py
@@ -11,34 +11,40 @@ def search():
     Args: [
         animal: {   def: "is the type of animal we want search.",
                     type: string,
+                    required: yes,
                     default: 'none',
                     values: ['cow', 'pig']
                 },
         farmId: {   def: number of farm where we want search this animal,
                     type: int,
+                    required: yes,
                     default: 0,
                     values: "any integer if it is greater than 0"
                 },
         animalId: { def: "identificator for a group of data",
                     type: string,
+                    required: yes,
                     default: 'none',
                     values: "many and variables",
                     comment: "this args have correlation with 'animalNum'."
                 },
         animalNum: { def: "number for a one animal (or group of animals)",
                     type: int,
+                    required: no,
                     default: 0,
                     values: "any integer if it is greater than 0",
                     comment: "this args have correlation with 'animalId'."
                 },
         quantity: { def: "how much information do you want about the animal or farm.",
                     type: string,
+                    required: no,
                     default: 'Last',
                     values: ['Last', 'All', 'Range'],
                     comment: "If you insert any value in 'timeFrom', the 'quantity' value are not important, because change automaticaly to 'Range'."
                 },
         timeFrom: { def: "lower limit for the range of dates where we want to search information",
                     type: string,
+                    required: no,
                     format date: 'dd/mm/yyyy',
                     default: 'none',
                     values: date,
@@ -46,6 +52,7 @@ def search():
                 },
         timeTo: {   def: "upper limit for the range of dates where we want to search information",
                     type: string,
+                    required: no,
                     format date: 'dd/mm/yyyy',
                     default: 'none',
                     values: "any date greater than 'timeFrom' and smaller than today",
@@ -62,6 +69,7 @@ def search():
     timeFrom = request.args.get('timeFrom', default = 'none', type = str)
     timeTo = request.args.get('timeTo', default = 'none', type = str)
     
+    #TODO: delete
     value = {
             "animal": animal,
             "farmId": farmId,
@@ -71,6 +79,10 @@ def search():
             "timeFrom": timeFrom,
             "timeTo": timeTo,
     }
+    
+    #Check values we need
+    
+    
     
     return value
 
