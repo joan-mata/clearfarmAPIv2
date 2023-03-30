78afbe8eafa0e6940d1fd7a855ea8af077751934
joan-mata <joan.mata.jmp@gmail.com>
2023-03-30 17:24:59 +0200

    from app import error_dict

--- app/__init__.py
+++ app/__init__.py
@@ -37,13 +37,65 @@ def create_app():
     
     return app
     
-    
-#    from .auth import auth_bp
-#    app.register_blueprint(auth_bp)
-#
-#    from .inserts import inserts_bp
-#    app.register_blueprint(inserts_bp)
 
+
+error_dict = {
+    "error_required_animal": {
+        "type": "error",
+        "name": "error_required_animal",
+        "text": "Is required the 'animal' value.",
+    },
+    "error_required_farmId": {
+        "type": "error",
+        "name": "error_required_farmId",
+        "text": "Is required the 'farmId' value or the value is not a integer.",
+    },
+    "error_value_farmId": {
+        "type": "error",
+        "name": "error_value_farmId",
+        "text": "Value is smaller than 1.",
+    },
+    "error_required_animalId": {
+        "type": "error",
+        "name": "error_required_animalId",
+        "text": "Is required the 'animalId' value because you have inserted the 'animalNum' value.",
+    },
+    "error_value_animalId": {
+        "type": "error",
+        "name": "error_value_animalId",
+        "text": "It is possible that you have not correctly selected the 'AnimalId' value.",
+    },
+    "error_format_timeFrom": {
+        "type": "error",
+        "name": "error_format_timeFrom",
+        "text": "Format of 'timeFrom' is not correct. Correct format is 'YYYY-MM-DD'.",
+    },
+    "error_format_timeTo": {
+        "type": "error",
+        "name": "error_format_timeTo",
+        "text": "Format of 'timeTo' is not correct. Correct format is 'YYYY-MM-DD'.",
+    },
+    "error_today_timeFrom": {
+        "type": "error",
+        "name": "error_today_timeFrom",
+        "text": "'timeFrom' value must be smaller than today's date."
+    },
+    "error_today_timeTo": {
+        "type": "error",
+        "name": "error_today_timeTo",
+        "text": "'timeTo' value must be smaller than today's date."
+    },
+    "error_small_timeTo": {
+        "type": "error",
+        "name": "error_small_timeTo",
+        "text": "'timeTo' value must be greater than 'timeFrom' value."
+    },
+    "no_data_animalNum": {
+        "type": "no_data",
+        "name": "no_data_animalNum",
+        "text": "No information about this animal in this farm"
+    },
+}
     
         
     
--- app/errors/resources/errors.py
+++ app/errors/resources/errors.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from app import error_dict
+
 from .. import errors_bp
 
 
@@ -12,63 +14,7 @@ def errors():
         None
     '''
     
-    error_dict = {
-        "error_required_animal": {
-            "type": "error",
-            "name": "error_required_animal",
-            "text": "Is required the 'animal' value.",
-        },
-        "error_required_farmId": {
-            "type": "error",
-            "name": "error_required_farmId",
-            "text": "Is required the 'farmId' value or the value is not a integer.",
-        },
-        "error_value_farmId": {
-            "type": "error",
-            "name": "error_value_farmId",
-            "text": "Value is smaller than 1.",
-        },
-        "error_required_animalId": {
-            "type": "error",
-            "name": "error_required_animalId",
-            "text": "Is required the 'animalId' value because you have inserted the 'animalNum' value.",
-        },
-        "error_value_animalId": {
-            "type": "error",
-            "name": "error_value_animalId",
-            "text": "It is possible that you have not correctly selected the 'AnimalId' value.",
-        },
-        "error_format_timeFrom": {
-            "type": "error",
-            "name": "error_format_timeFrom",
-            "text": "Format of 'timeFrom' is not correct. Correct format is 'YYYY-MM-DD'.",
-        },
-        "error_format_timeTo": {
-            "type": "error",
-            "name": "error_format_timeTo",
-            "text": "Format of 'timeTo' is not correct. Correct format is 'YYYY-MM-DD'.",
-        },
-        "error_today_timeFrom": {
-            "type": "error",
-            "name": "error_today_timeFrom",
-            "text": "'timeFrom' value must be smaller than today's date."
-        },
-        "error_today_timeTo": {
-            "type": "error",
-            "name": "error_today_timeTo",
-            "text": "'timeTo' value must be smaller than today's date."
-        },
-        "error_small_timeTo": {
-            "type": "error",
-            "name": "error_small_timeTo",
-            "text": "'timeTo' value must be greater than 'timeFrom' value."
-        },
-        "no_data_animalNum": {
-            "type": "no_data",
-            "name": "no_data_animalNum",
-            "text": "No information about this animal in this farm"
-        },
-    }
+    
     
     return error_dict
 
