4299e9803ae3f01595ce9c69ac9b07b1baf69de6
joan-mata <joan.mata.jmp@gmail.com>
2023-03-30 18:00:15 +0200

    /insert coments

--- app/insert/resources/insert.py
+++ app/insert/resources/insert.py
@@ -8,10 +8,40 @@ from .. import insert_bp
 @insert_bp.route('/insert', methods=['POST'])
 def insert():
     '''
-    Inserts.
+    Insert data n REST-API.
+    If mandatory you use POST method.
     
-    Args:
-        None
+    Args: {
+        animal: {
+            def: "is the type of animal we want insert.",
+            type: string,
+            required: yes,
+            values: ['cow', 'pig']
+        },
+        enterprise: {
+            def: "is the organism that provides the data.",
+            type: string,
+            required: yes,
+            values: undefined
+        },
+        file: {
+            def: "is the data.",
+            type: string,
+            required: yes,
+            values: ['csv', 'json']
+        },
+        key: {
+            def: "ID of this enterprise.",
+            type: string,
+            required: "depending (if your insert this enterprise for first time, 'key' is required",
+            value: undefined
+        }
+    }
+    
+    Return: {
+        error_no_post: "if you do not use POST method.",
+        ok_insert: "if you insert the data correctly."
+    }
     '''
     
     value_return = msg_dict["error_no_post"]
