746f73354ecf075ad95225271bc0b78ce38bc129
joan-mata <joan.mata.jmp@gmail.com>
2023-04-17 19:50:50 +0200

    /welfare/health return data ¿what data? -> toArray()

--- app/welfare/functions/welfareLastCow.py
+++ app/welfare/functions/welfareLastCow.py
@@ -18,7 +18,7 @@ def welfareLastCow(walfare, animalNum):
     '''
     walfare_value = walfare + "_score"
     
-    data = db_cows["welfare"].find_many({"cowID": animalNum},{"_id": 0, walfare_value: 1,  "cowID": 1, "date": 1})
+    data = db_cows["welfare"].find({"cowID": animalNum},{"_id": 0, walfare_value: 1,  "cowID": 1, "date": 1}).toArray()
     
     
     
