ce031de43e6023b5c008f03ae0d8e02352fa1689
joan-mata <joan.mata.jmp@gmail.com>
2023-04-26 22:03:31 +0200

    /welfare/all test1

--- app/welfare/resources/welfare.py
+++ app/welfare/resources/welfare.py
@@ -220,4 +220,64 @@ def wf_global():
     value_return = funct_welfare.funct_welfare("global", animal, farmId, animalNum, timeFrom, timeTo)
     
     return value_return
-            
+
+@welfare_bp.route('/welfare/all')
+def wf_global():
+    '''
+    Search and return data range for GLOBAL score of an animal.
+    
+    Args: {
+        animal: {   def: "is the type of animal we want search.",
+                    type: string,
+                    required: yes,
+                    default: 'none',
+                    values: ['cow', 'pig']
+                },
+        farmId: {   def: number of farm where we want search this animal,
+                    type: int,
+                    required: yes,
+                    default: 0,
+                    values: "any integer if it is greater than 0"
+                },
+        animalNum: { def: "number for a one animal (or group of animals)",
+                    type: int,
+                    required: no,
+                    default: 0,
+                    values: "any integer if it is greater than 0",
+                },
+        timeFrom: { def: "lower limit for the range of dates where we want to search information",
+                    type: string,
+                    required: no,
+                    format date: 'YYYY-MM-DD',
+                    default: 'none',
+                    values: date,
+                    comment: "If you insert any value in 'timeFrom', the 'quantity' value are not important, because change automaticaly to 'Range'."
+                },
+        timeTo: {   def: "upper limit for the range of dates where we want to search information",
+                    type: string,
+                    required: no,
+                    format date: 'YYYY-MM-DD',
+                    default: 'none',
+                    values: "any date greater than 'timeFrom' and smaller than today",
+                    comment: "If you do not insert any value in 'timeFrom', the 'timeTo' does not work."
+                },
+    }
+    '''
+    
+    animal = request.args.get('animal', default = 'none', type = str)
+    farmId = str(request.args.get('farmId', default = 0, type = int))
+    animalNum = str(request.args.get('animalNum', default = 0, type = int))
+    timeFrom = request.args.get('timeFrom', default = 'none', type = str)
+    timeTo = request.args.get('timeTo', default = 'none', type = str)
+    #Check values we need
+    #Values are required: animal, farmId, *animalId
+    
+    welfare = ["health", "feeding", "housing", "global"]
+    value_return = []
+    
+    for item in welfare:
+        aux_data = funct_welfare.funct_welfare(item, animal, farmId, animalNum, timeFrom, timeTo)
+        dict = {item: aux_data}
+        value_return.append(dict)
+    
+    return value_return
