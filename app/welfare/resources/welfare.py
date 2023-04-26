dfcf501d93ef2b1c7138e5dff3d3bbfdb9965572
joan-mata <joan.mata.jmp@gmail.com>
2023-04-26 21:33:36 +0200

    /welfare/health repair function tree 2

--- app/welfare/resources/welfare.py
+++ app/welfare/resources/welfare.py
@@ -4,6 +4,7 @@ from app import db_cows
 from app import msg_dict
 
 from .. import welfare_bp
+from ..functions import funct_welfare
 from ..functions import welfareLastCow
 from ..functions import welfareLastFarm
 from ..functions import welfareRangeCow
@@ -59,40 +60,8 @@ def wf_health():
     timeTo = request.args.get('timeTo', default = 'none', type = str)
     #Check values we need
     #Values are required: animal, farmId, *animalId
-    value_return = funct_welfare("health", animal, farmId, animalNum, timeFrom, timeTo)
+    value_return = funct_welfare.funct_welfare("health", animal, farmId, animalNum, timeFrom, timeTo)
     return value_return
-
-def funct_welfare(welfare, animal, farmId, animalNum, timeFrom, timeTo):
-    quantity = "Last"
-    #Check if you have inserted timeFrom
-    if timeFrom != 'none':
-        quantity = "Range"
-    
-    #Check values to animal
-    if animal == "none":
-        value_return = msg_dict["error_required_animal"]
-    #Check values to farmId
-    elif int(farmId) <= 0:
-        if farmId == "0":
-            value_return = msg_dict["error_required_farmId"]
-        else:
-            value_return = msg_dict["error_value_farmId"]
-    #Check if you have inserted animalNum
-    elif animalNum != "0":
-        #Search concrete animal
-        if quantity == "Last":
-            value_return = welfareLastCow.welfareLastCow(welfare, animalNum)
-        else:
-            value_return = welfareRangeCow.welfareRangeCow(welfare, animalNum, timeFrom, timeTo)
-    #Search all animals in farm
-    else:
-        if quantity == "Last":
-            value_return = welfareLastFarm.welfareLastFarm(welfare, farmId)
-        else:
-            value_return = welfareRangeFarm.welfareRangeFarm(welfare, farmId, timeFrom, timeTo)
-        
-    return value_return
-
 
 @welfare_bp.route('/welfare/feeding')
 def wf_feeding():
