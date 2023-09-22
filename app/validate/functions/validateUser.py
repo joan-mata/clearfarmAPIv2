bf1fe45e4afe19d0a2b6a9ab6205221fc6098509
MCarmen <detoro.mcarmen@gmail.com>
2023-09-22 10:03:40 +0200

    The login information is now a json.

--- app/validate/functions/validateUser.py
+++ app/validate/functions/validateUser.py
@@ -1,22 +1,21 @@
 from . import searchUser
 import base64
 
-def validateUser(user, hash):
+def validateUser(loginInfo):
     '''
     Validates if the stored user's hash matches with the form-provided one
     
-    :param user: the user's dictionary from the database
-    :param hash: the user's password's hash encoded as base64
-    :return: true if the stored user's password's hashcode matches with the 
-    provided one.
+    :param loginInfo: Json including the user login and password's hash.
+    :return: True if the stored user's password's hashcode matches with the 
+    provided one, False otherwise.
     '''
-    query_result = searchUser(user)
+    query_result = searchUser(loginInfo['user'])
 
     validated = True if len(query_result) and query_result[0]['hash']==hash else False
     #decoded_input_hash = base64.b64decode(hash)
-    print(f'user hash input: {hash}') #debug
+    print(f"user hash input: {loginInfo['hash']}") #debug
     if len(query_result):
-      print('db hash: {}'.format(query_result[0]['hash'])) #debug
-      print('Are equal? {}'.format(query_result[0]['hash']==hash))#debug
+      print(f"db hash: {query_result[0]['hash']}") #debug
+      print(f"Are equal? {query_result[0]['hash'] == loginInfo['hash']}")#debug
 
     return validated
