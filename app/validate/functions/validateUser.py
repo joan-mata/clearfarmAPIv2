5c09e89b070d94a0964a5666e38d0e21e0296e09
MCarmen <detoro.mcarmen@gmail.com>
2023-09-22 13:29:40 +0200

    Search result is a dictionary not an array.

--- app/validate/functions/validateUser.py
+++ app/validate/functions/validateUser.py
@@ -9,13 +9,13 @@ def validateUser(loginInfo):
     :return: True if the stored user's password's hashcode matches with the 
     provided one, False otherwise.
     '''
-    query_result = searchUser.searchUser(loginInfo['user'])
+    user = searchUser.searchUser(loginInfo['user'])
 
-    validated = True if len(query_result) and query_result[0]['hash']==hash else False
+    validated = True if len(user) and user['hash']==hash else False
     #decoded_input_hash = base64.b64decode(hash)
     print(f"user hash input: {loginInfo['hash']}") #debug
-    if len(query_result):
-      print(f"db hash: {query_result[0]['hash']}") #debug
-      print(f"Are equal? {query_result[0]['hash'] == loginInfo['hash']}")#debug
+    if len(user): #debug
+      print(f"db hash: {user['hash']}") #debug
+      print(f"Are equal? {user['hash'] == loginInfo['hash']}")#debug
 
     return validated
