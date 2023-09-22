f265436062bc64f25f64c4088dd6ffa1139e0889
MCarmen <detoro.mcarmen@gmail.com>
2023-09-22 13:30:31 +0200

    Debug.

--- app/validate/functions/searchUser.py
+++ app/validate/functions/searchUser.py
@@ -10,10 +10,10 @@ def searchUser(login):
     :return: true if the user's password's hashcode 
     '''
     users = list(db_users["admin"].find({'user': login})) #debug
-    print(f'user: {users}') #debug
+    #print(f'user: {users}') #debug
     query_result = list(db_users["admin"].find({'user': login}))
     user = query_result[0] if len(query_result) else {}
-    print(f'query_result_0 {query_result[0]}') #debug
-    print(f'len query_result {len(query_result)}') #debug
+    #print(f'query_result_0 {query_result[0]}') #debug
+    #print(f'len query_result {len(query_result)}') #debug
     print(f'user {user}') #debug
     return user
