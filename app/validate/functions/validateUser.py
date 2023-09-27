324c721bbba47000063daa92dec279b17b2cf06e
MCarmen <detoro.mcarmen@gmail.com>
2023-09-27 17:34:25 +0200

    If the user is found the whole user register is retuned, otherwise an empty dictionary is returned.

--- app/validate/functions/validateUser.py
+++ app/validate/functions/validateUser.py
@@ -9,7 +9,7 @@ def validateUser(loginInfo):
     user has been found (1) and if the stored user's password's hashcode matches with the 
     provided one (1).
     '''
-    result = {'found': 0, 'validated': 0}
+    
     user = searchUser.searchUser(loginInfo['user'])
     print(f'db_ user {user}')
     if len(user):
@@ -18,7 +18,6 @@ def validateUser(loginInfo):
 
       print(f"Are equal: \n {input_pwd_to_hex} \n {user['password']} \n {user['password'] == input_pwd_to_hex}")
 
-      result['found']=1
-      result['validated'] = 1 if user['password']==input_pwd_to_hex else 0
+      result = user if user['password']==input_pwd_to_hex else {}
     
     return result
