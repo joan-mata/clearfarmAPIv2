0788906f38f28f101299322cc2e595223bc35204
MCarmen <detoro.mcarmen@gmail.com>
2023-10-03 13:24:11 +0200

    Correcting indentation.

--- app/disease/functions/diseaseLastCow.py
+++ app/disease/functions/diseaseLastCow.py
@@ -25,32 +25,31 @@ def diseaseLastCow(disease, animalNum):
     item_date = [0, 0, 0] #[mm, dd, yy]
     for item in data:
         if item[disease] != "":
+          date_aux = item["dateStart"]
+          
+          value1 = date_aux.index("/")
+          value2 = date_aux[value1+1:].index("/") + value1 + 1
+          
+          item_date[0] = int(date_aux[:value1])
+          item_date[1] = int(date_aux[value1+1:value2])
+          item_date[2] = int(date_aux[value2+1:])
 
-            date_aux = item["dateStart"]
-            
-            value1 = date_aux.index("/")
-            value2 = date_aux[value1+1:].index("/") + value1 + 1
-            
-            item_date[0] = int(date_aux[:value1])
-            item_date[1] = int(date_aux[value1+1:value2])
-            item_date[2] = int(date_aux[value2+1:])
-
-            #analyze year
-            if item_date[2] > date[2]:
-                date[0] = item_date[0]
-                date[1] = item_date[1]
-                date[2] = item_date[2]
-                return_data = item[disease]
-            elif item_date[2] == date[2]:
-                #analyze month
-                if item_date[0] > date[0]:
-                    date[0] = item_date[0]
-                    date[1] = item_date[1]
-                    return_data = item[disease]
-                elif item_date[0] == date[0]:
-                    #analyze day
-                    if item_date[1] > date[1]:
-                        date[1] = item_date[1]
-                        return_data = item[disease]
+          #analyze year
+          if item_date[2] > date[2]:
+              date[0] = item_date[0]
+              date[1] = item_date[1]
+              date[2] = item_date[2]
+              return_data = item[disease]
+          elif item_date[2] == date[2]:
+              #analyze month
+              if item_date[0] > date[0]:
+                  date[0] = item_date[0]
+                  date[1] = item_date[1]
+                  return_data = item[disease]
+              elif item_date[0] == date[0]:
+                  #analyze day
+                  if item_date[1] > date[1]:
+                      date[1] = item_date[1]
+                      return_data = item[disease]
     
     return return_data
